e0c69190f192e0961ac627460e42da228ea5aa26
Kevin <dic15kze@student.lu.se>
2018-04-16 23:16:44 +0200

    Initial commit

---
+++ app/src/main/java/com/souschef/sork/sous_chef/IntroActivity.java
@@ -0,0 +1,13 @@
+package com.souschef.sork.sous_chef;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
