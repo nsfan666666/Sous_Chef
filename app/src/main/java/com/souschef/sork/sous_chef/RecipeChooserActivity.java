6d659828dac0265137dc7c99a18e241c4581c39d
Hackerlacka <robin.robbe96@gmail.com>
2018-04-24 14:02:39 +0200

    Added example progress bar for cooking instructions

--- app/src/main/java/com/souschef/sork/sous_chef/RecipeChooserActivity.java
+++ app/src/main/java/com/souschef/sork/sous_chef/RecipeChooserActivity.java
@@ -21,6 +21,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -137,6 +138,7 @@ public class RecipeChooserActivity extends AppCompatActivity {
                         @Override
                         public void onClick(View v) {
                             // Start cooking activity
+                            /*
                             Intent intent = new Intent(getContext(), CookingActivity.class);
                             Bundle args = new Bundle();
 
@@ -144,6 +146,10 @@ public class RecipeChooserActivity extends AppCompatActivity {
                             args.putSerializable(RECIPE_LITE, recipe.getLite());
 
                             intent.putExtras(args);
+                            startActivity(intent);
+                            */
+
+                            Intent intent = new Intent(getContext(), ProgressBarTest.class);
                             startActivity(intent);
                         }
                     });
