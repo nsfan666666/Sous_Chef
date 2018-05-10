18bab7270fb3994f5f7db8fa8ad2bfc671eed7c2
Robin <robin.robbe96@gmail.com>
2018-05-10 15:25:36 +0200

    Improvements to timer

--- app/src/main/java/com/souschef/sork/sous_chef/Timer.java
+++ app/src/main/java/com/souschef/sork/sous_chef/Timer.java
@@ -2,10 +2,12 @@ package com.souschef.sork.sous_chef;
 
 import android.content.Context;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by Robin on 2018-05-10.
@@ -19,6 +21,10 @@ public class Timer{
     private long totalDurationMilliseconds;
     private long timeLeftMilliseconds;
 
+    private TextView countdownTimerTextView;
+    private Button start;
+    private Button reset;
+
     public Timer(Context baseContext, long totalDurationMilliseconds) {
         this.totalDurationMilliseconds = totalDurationMilliseconds;
         timeLeftMilliseconds = totalDurationMilliseconds;
@@ -26,27 +32,30 @@ public class Timer{
         LayoutInflater inflater = LayoutInflater.from(baseContext);
         rootView = (LinearLayout) inflater.inflate(R.layout.countdown_timer_layout, null, false);
 
-        final Button start = (Button) rootView.findViewById(R.id.start_countdown_timer);
+        countdownTimerTextView = (TextView) rootView.findViewById(R.id.countdown_timer);
+        countdownTimerTextView.setText(getTimeRemaining());
+
+        start = (Button) rootView.findViewById(R.id.start_countdown_timer);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(timerRunning) {
-                    ((Button) v).setText("Start");
                     pause();
+                    timerRunning = false;
+                    ((Button) v).setText("Start");
                 } else {
-                    ((Button) v).setText("Pause");
                     start();
+                    timerRunning = true;
+                    ((Button) v).setText("Pause");
                 }
             }
         });
-        Button stop = (Button) rootView.findViewById(R.id.reset_countdown_timer);
-        stop.setOnClickListener(new View.OnClickListener() {
+        reset = (Button) rootView.findViewById(R.id.reset_countdown_timer);
+        reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(timerRunning) {
-                    reset();
-                    ((Button) v).setVisibility(View.INVISIBLE);
-                }
+                reset();
+                ((Button) v).setVisibility(View.INVISIBLE);
             }
         });
     }
@@ -64,14 +73,17 @@ public class Timer{
                 @Override
                 public void onTick(long millisUntilFinished) {
                     timeLeftMilliseconds = millisUntilFinished;
+                    countdownTimerTextView.setText(getTimeRemaining());
                 }
 
                 @Override
                 public void onFinish() {
-                    // TODO
+                    // TODO Make sound
 
                 }
             };
+            countDownTimer.start();
+            reset.setVisibility(View.VISIBLE);
         }
     }
 
@@ -80,8 +92,21 @@ public class Timer{
     }
 
     public void reset() {
+        timerRunning = false;
         countDownTimer.cancel();
         timeLeftMilliseconds = totalDurationMilliseconds;
+        countdownTimerTextView.setText(getTimeRemaining());
+        start.setText("Start");
+    }
+
+    public String getTimeRemaining() {
+        long tempDuration = timeLeftMilliseconds / 1000;
+        long hours = tempDuration / 3600;
+        tempDuration = tempDuration % 3600;
+        long minutes = tempDuration / 60;
+        tempDuration = tempDuration % 60;
+        long seconds = tempDuration;
+        return String.format("%02d:%02d:%02d",hours, minutes, seconds);
     }
 
 }
